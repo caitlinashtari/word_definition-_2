b104064ec92daeed6ba717da4c7f71beabf1b6ef
Caitlin Ashtari <caitlinashtari@gmail.com>
2016-12-02 08:26:09 -0800

    create initialize method to set class Word attributes and 1 passing spec.

--- lib/word.rb
+++ lib/word.rb
@@ -1,3 +1,12 @@
 class Word
+  @@words = []
+
+  attr_reader :word, :definition, :id
+
+  define_method(:initialize) do |attributes|
+    @word = attributes.fetch(:word)
+    @definition = attributes.fetch(:definition)
+    @id = @@words.length + 1
+  end
 
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,6 +1,18 @@
 require('rspec')
 require('word')
-require('definition')
+# require('definition')
 
 
-describe(Contact) do
+describe(Word) do
+  describe("#initialize") do
+    it('creates a new Word object and sets its attributes') do
+      test_word = Word.new({:word=> "incipient", :definition=> "in an initial stage"})
+      expect(test_word.word).to(eq("incipient"))
+      expect(test_word.definition).to(eq("in an initial stage"))
+      expect(test_word.id).to(eq(1))
+    end
+  end
+
+
+
+end
