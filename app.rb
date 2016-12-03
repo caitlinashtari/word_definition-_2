fb4dfcac2a6e42c5c2afc25d5d49b0148d18aed3
Caitlin Ashtari <caitlinashtari@gmail.com>
2016-12-03 12:56:56 -0800

    debug routes and add funtionality to add a second definition.

--- app.rb
+++ app.rb
@@ -19,12 +19,28 @@ post('/word') do
   @new_definition = Definition.new({:definition=> @definition, :speach_part=> @speach_part})
   @new_definition.save()
   @dictionary = Word.all()
+  @new_word.add_definition(@new_definition)
   erb(:success)
 end
 
 get('/word/:id') do
-  @displayed_word = Word.find(params.fetch('id').to_i())
+  @displayed_word_id = params.fetch('id').to_i()
+  @displayed_word = Word.find(@displayed_word_id)
   @displayed_speach = Definition.find(params.fetch('id').to_i())
-  @displayed_definition = Definition.find(params.fetch('id').to_i())
   erb(:word_view)
+end
+
+get('/word/:id/definition_new') do
+  @new_definition_word_id = params.fetch('id').to_i
+  erb(:definition_new)
+end
+
+post('/definition_new') do
+  @word_id = params.fetch("new_definition_word_id").to_i
+  @second_definition_word = params.fetch('second_definition')
+  @second_speach_word = params.fetch('second_speach')
+  @second_definition = Definition.new({:definition=> @second_definition_word, :speach_part=> @second_speach_word})
+  @word = Word.find(@word_id)
+  @word.add_definition(@second_definition)
+  erb(:success)
 end
